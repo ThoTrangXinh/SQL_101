9c18f1ea14438e43bb818d7c74907d70831390df
ThoTrangXinh <143240780+ThoTrangXinh@users.noreply.github.com>
2024-04-04 16:06:14 +0700

    Update Day9_Practice4.sql

--- Day9_Practice4.sql
+++ Day9_Practice4.sql
@@ -1,1 +1,22 @@
+-- EXERCISE 1
+select
+sum(case
+    when device_type = 'laptop' then 1
+    else 0
+    end) as laptop_views,
+sum(case
+    when device_type = 'tablet' or device_type = 'phone' then 1
+    else 0
+    end) as mobile_views
+from viewership
+
+-- EXERCISE 2
+select *,
+case
+    when x+y>z and y+z>x and z+x>y then 'Yes'
+    else 'No'
+end as triangle
+from Triangle
+
+-- EXERCISE 3
 
