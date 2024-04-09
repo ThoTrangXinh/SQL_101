945ff81e7b2e1a87ef4588bc1b7fa9c1d5647a8d
ThoTrangXinh <143240780+ThoTrangXinh@users.noreply.github.com>
2024-04-09 12:54:12 +0700

    Update Day9_Practice4.sql

--- Day9_Practice4.sql
+++ Day9_Practice4.sql
@@ -19,4 +19,28 @@ end as triangle
 from Triangle
 
 -- EXERCISE 3
+SELECT
+cast(sum (CASE
+    when call_category = 'n/a' or call_category is NULL then 1
+    else 0
+    end) as decimal) /count (case_id)
+FROM callers
 
+-- EXERCISE 4
+select name from customer
+where referee_id is null
+or referee_id <> 2
+
+-- EXERCISE 5
+select survived,
+sum(case
+    when pclass = 1 then 1
+    end) as first_class,
+sum(case
+    when pclass = 2 then 1
+    end) as second_classs,
+sum(case
+    when pclass = 3 then 1
+    end) as third_class
+from titanic
+group by survived
